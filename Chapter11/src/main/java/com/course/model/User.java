5e83877b98d23700a1af190e535886a2952a71da
Pofe666 <162188190@qq.com>
2020-03-25 12:53:27 +0800

    提交代码

---
+++ Chapter11/src/main/java/com/course/model/User.java
@@ -0,0 +1,16 @@
+package com.course.model;
+
+import lombok.Data;
+
+/**
+ * @author 86186
+ * @date 2020/3/24 23:02
+ * @Description
+ */
+@Data
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private String sex;
+}
